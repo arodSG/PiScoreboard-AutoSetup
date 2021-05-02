3d0552f731f44e6e55fe384759b13a95edafa863
andrewpr52 <andrewpr52@gmail.com>
2021-05-02 13:24:44 -0500

    Changes for more automation

--- autosetup.sh
+++ autosetup.sh
@@ -8,9 +8,15 @@ git clone --recursive https://github.com/MLB-LED-Scoreboard/mlb-led-scoreboard
 cd mlb-led-scoreboard/
 sudo ./install.sh
 
+# move script folder
+sudo cp /home/pi/PiScoreboard-AutoSetup/scripts /home/pi/scripts
+
 # set up service so scoreboard will run automatically on boot
-sudo mv mlb-led-scoreboard.service /etc/systemd/system/mlb-led-scoreboard.service
+sudo cp /home/pi/PiScoreboard-AutoSetup/service/mlb-led-scoreboard.service /etc/systemd/system/mlb-led-scoreboard.service
 sudo systemctl enable mlb-led-scoreboard.service
+
+# create crontab (for pi user) to restart scoreboard automatically every day at 6am
+sudo cp /home/pi/PiScoreboard-AutoSetup/crontab/pi /var/spool/cron/crontabs/pi
 
 # install Adafruit RGB matrix software (this step will disable the sound module and eliminate LED flickering)
 cd /home/pi
