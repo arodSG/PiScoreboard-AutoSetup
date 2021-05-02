38d7b8c766b037ca58f4e5fcbaebe01a97c85cae
andrewpr52 <andrewpr52@gmail.com>
2021-05-02 11:57:51 -0500

    Added -y flags to automate apt-get commands

--- autosetup.sh
+++ autosetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git python-pip
+sudo apt-get update -y
+sudo apt-get install -y git python-pip
 
 cd /home/pi
 git clone --recursive https://github.com/MLB-LED-Scoreboard/mlb-led-scoreboard
